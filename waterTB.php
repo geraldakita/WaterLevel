f9ff5a5a23f53d46fadfe310f6295d124e0be17e
Eben <79762026+eakolly@users.noreply.github.com>
2022-10-28 10:56:57 +0000

    Inserting into the water table

---
+++ waterTB.php
@@ -0,0 +1,21 @@
+<?php
+$servername= "localhost";
+$username="root";
+$password="";
+$dbname="waterTank";
+$con = mysqli_connect($servername,$username,$password,$dbname);
+
+$water_level=$_GET['water_level'];
+$location = $_GET['location'];
+$ownerID = $_GET['ownerID'];
+
+$sql = "INSERT INTO waterDetails (water_level, location, ownerID)
+        VALUES ('{$water_level}','{$location}','{$ownerID}')";
+
+if(mysqli_query($con,$sql)){
+    echo "New record created successfully";
+} else {
+    echo "Failed to record";
+    }
+?>
+
